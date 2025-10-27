d48a39230be4e90be8f2c38a6173416479cbfff9
Chuah Hui Wen <chwen0912@gmail.com>
2025-10-27 15:57:43 +0800

    Add files via upload

---
+++ worker.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#include <string>
+
+class Worker{
+    public:
+    virtual void showInfo() = 0; 
+    virtual string getPosition() = 0;
+    int id;
+    string name;
+    int deptid;
+};
